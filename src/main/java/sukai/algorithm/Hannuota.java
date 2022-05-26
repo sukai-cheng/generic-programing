068998a4b09da2f1a58154dc571913a7585bf70c
chengsukai <1710290860@qq.com>
2022-05-26 09:32:36 +0800

    LearnNewThing: Algorithm: recursion

---
+++ src/main/java/sukai/algorithm/Hannuota.java
@@ -0,0 +1,20 @@
+package sukai.algorithm;
+
+/**
+ * @author chengsukai
+ **/
+public class Hannuota {
+    public static void main(String[] args) {
+        hanoi("a", "b", "c", 2);
+    }
+
+    public static void hanoi(String a, String b, String c, int N) {
+        if (N == 1) {
+            System.out.println(a + " -> " + c);
+        } else {
+            hanoi(a, c, b, N - 1);
+            System.out.println(a + " -> " + c);
+            hanoi(b, a, c, N - 1);
+        }
+    }
+}
