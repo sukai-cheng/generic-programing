0e3e5aea204368f0b80843d85d3f11343f478030
sukai-cheng <1710290860@qq.com>
2022-06-04 11:20:45 +0800

    Fix: DP: Add new comment

---
+++ src/main/java/sukai/desginpattern/simpleFactory/caseII/Factory.java
@@ -0,0 +1,7 @@
+package sukai.desginpattern.simpleFactory.caseII;
+
+import sukai.desginpattern.simpleFactory.caseI.Product;
+
+public abstract class Factory {
+    public abstract Product Manufacture();
+}
