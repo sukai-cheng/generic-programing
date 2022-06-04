fb6dd5fdc349f667a0ac1572a1ad00deb3e3a425
sukai-cheng <1710290860@qq.com>
2022-06-04 11:19:39 +0800

    Fix: DP: StaticFactoryPattern

--- src/main/java/sukai/desginpattern/simpleFactory/caseI/SimpleFactoryPattern.java
+++ src/main/java/sukai/desginpattern/simpleFactory/caseI/SimpleFactoryPattern.java
@@ -1,11 +1,12 @@
 package sukai.desginpattern.simpleFactory.caseI;
 
+import java.util.Objects;
+
 public class SimpleFactoryPattern {
     public static void main(String[] args) {
-        Factory mFactory = new Factory();
 
         try{
-            mFactory.Manufacture("A").show();
+            Objects.requireNonNull(Factory.Manufacture("A")).show();
         }catch (NullPointerException e){
             System.out.println("没有这一类产品");
         }
