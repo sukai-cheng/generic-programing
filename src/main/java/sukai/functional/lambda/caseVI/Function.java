097b9928328a775263648cbfcea70cb6d510a4d1
chengsukai <1710290870@qq.com>
2022-08-08 13:36:53 +0800

    chore: add something

---
+++ src/main/java/sukai/functional/lambda/caseVI/Function.java
@@ -0,0 +1,9 @@
+package sukai.functional.lambda.caseVI;
+
+/**
+ * @author chengsukai
+ * @since 2022-08-06 14:25
+ */
+public interface Function <T,U>{
+    U apply(T arg);
+}
---
+++ src/main/java/sukai/functional/lambda/caseVI/sum.java
@@ -0,0 +1,9 @@
+package sukai.functional.lambda.caseVI;
+
+/**
+ * @author chengsukai
+ * @since 2022-08-06 16:45
+ */
+public interface sum {
+
+}
