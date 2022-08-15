0d36292dd68a367b093dc45f4d44c3d2c746ee61
chengsukai <1710290870@qq.com>
2022-08-15 09:57:02 +0800

    chore: completeFuture学习

---
+++ src/main/java/sukai/currency/caseVI/_01_supplyAsync.java
@@ -0,0 +1,25 @@
+package sukai.currency.caseVI;
+
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * @author chengsukai
+ * @since 2022-08-15 09:44
+ */
+public class _01_supplyAsync {
+    public static void main(String[] args) {
+        SmallTool.printTimeAndThread("小白进入餐厅");
+        SmallTool.printTimeAndThread("小白点了 番茄炒蛋 + 一碗米饭");
+
+        CompletableFuture<String> cf1 = CompletableFuture.supplyAsync(() -> {
+            SmallTool.printTimeAndThread("厨师炒菜");
+            SmallTool.sleepMills(200);
+            SmallTool.printTimeAndThread("厨师打饭");
+            SmallTool.sleepMills(100);
+            return "番茄炒蛋 + 米饭 做好了";
+        });
+
+        SmallTool.printTimeAndThread("小白在打王者");
+        SmallTool.printTimeAndThread(String.format("%s,小白开吃", cf1.join()));
+    }
+}
