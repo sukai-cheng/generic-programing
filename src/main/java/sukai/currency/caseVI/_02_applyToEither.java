f305f658af2773796a7d4ed2602cb812f134042e
chengsukai <1710290870@qq.com>
2022-08-15 10:46:08 +0800

    chore: completeFuture-applyToEither

--- src/main/java/sukai/currency/caseVI/_02_applyToEither.java
+++ src/main/java/sukai/currency/caseVI/_02_applyToEither.java
@@ -8,20 +8,20 @@ import java.util.concurrent.CompletableFuture;
  */
 public class _02_applyToEither {
     public static void main(String[] args) {
-        SmallTool.printTimeAndThread("小白走出餐厅，来到公交站");
-        SmallTool.printTimeAndThread("等待 700路 或者 800路 公交到来");
 
+        SmallTool.printTimeAndThread("建立连接中...");
         CompletableFuture<String> bus = CompletableFuture.supplyAsync(() -> {
-            SmallTool.printTimeAndThread("700路公交正在赶来");
-            SmallTool.sleepMills(100);
-            return "700路到了";
+            SmallTool.sleepMills(1000);
+            SmallTool.printTimeAndThread("连接超时");
+            return "结束通话";
         }).applyToEither(CompletableFuture.supplyAsync(() -> {
-            SmallTool.printTimeAndThread("800路公交正在赶来");
-            SmallTool.sleepMills(200);
-            return "800路到了";
+            SmallTool.sleepMills(1200);
+            SmallTool.printTimeAndThread("连接成功");
+            SmallTool.printTimeAndThread("开始通话");
+            return "开始通话";
             // 直接把先来的那人任务的结果返回
         }), firstComeBus -> firstComeBus);
 
-        SmallTool.printTimeAndThread(String.format("%s,小白坐车回家", bus.join()));
+        SmallTool.printTimeAndThread(String.format("%s,------------，结束通话", bus.join()));
     }
 }
