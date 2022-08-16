f99fbae47f19ae9c2f8e8e5287d2c5dba2fa6f64
chengsukai <1710290870@qq.com>
2022-08-16 15:02:02 +0800

    chore: 修改commonpool来提高线程并发数

--- src/main/java/sukai/currency/caseVII/_01_terribleCode.java
+++ src/main/java/sukai/currency/caseVII/_01_terribleCode.java
@@ -2,8 +2,9 @@ package sukai.currency.caseVII;
 
 import sukai.currency.caseVI.SmallTool;
 
-import java.util.ArrayList;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ForkJoinPool;
+import java.util.stream.IntStream;
 
 /**
  * @author chengsukai
@@ -11,19 +12,40 @@ import java.util.concurrent.CompletableFuture;
  */
 public class _01_terribleCode {
     public static void main(String[] args) {
+
+//        System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism", "10");
+        // 最好使用自定义线程池来做
         SmallTool.printTimeAndThread("小白和小伙伴们 进餐厅点菜");
         long startTime = System.currentTimeMillis();
+//
+//        ArrayList<Dish> dishes = new ArrayList<>();
+//
+//        for (int i = 1; i <= 10; i++) {
+//            Dish dish = new Dish("菜" + i, 1);
+//            dishes.add(dish);
+//        }
+//
+//        ArrayList<CompletableFuture> cflist = new ArrayList<>();
+//        for (Dish dish : dishes) {
+//            CompletableFuture<Void> cf = CompletableFuture.runAsync(() -> dish.make());
+//            cflist.add(cf);
+//        }
+//
+//        CompletableFuture.allOf(cflist.toArray(new CompletableFuture[cflist.size()])).join();
 
-        ArrayList<Dish> dishes = new ArrayList<>();
+        // return the number of processors available to the Java virtual machine
+        System.out.println(Runtime.getRuntime().availableProcessors());
+        // return current size of thread num
+        System.out.println(ForkJoinPool.commonPool().getPoolSize());
+        // return max thread num
+        System.out.println(ForkJoinPool.getCommonPoolParallelism());
+        CompletableFuture[] dishes = IntStream.rangeClosed(1, 10)
+                .mapToObj(i -> new Dish("菜" + i, 1)).
+                map(dish -> CompletableFuture.runAsync(dish::make)).
+                toArray(size -> new CompletableFuture[size]);
 
-        for (int i = 1; i <= 10; i++) {
-            Dish dish = new Dish("菜" + i, 1);
-            dishes.add(dish);
-        }
+        CompletableFuture.allOf(dishes).join();
 
-        for (Dish dish : dishes) {
-            CompletableFuture.runAsync(() -> dish.make()).join(); // 直接将异步任务变成了串行
-        }
 
         SmallTool.printTimeAndThread("菜都做好了，上桌" + (System.currentTimeMillis() - startTime));
     }
