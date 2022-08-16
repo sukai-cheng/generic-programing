0ce9771b9903171e4e686452b0ad62a8cbd970c9
chengsukai <1710290870@qq.com>
2022-08-16 17:46:16 +0800

    chore: 生产者消费者

---
+++ src/main/java/sukai/currency/caseVI/_01_Queue_demo.java
@@ -0,0 +1,23 @@
+package sukai.currency.caseVI;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author chengsukai
+ * @since 2022-08-16 17:31
+ */
+public class _01_Queue_demo {
+    public static void main(String[] args) {
+        Queue<String> queue = new LinkedList<>();
+        queue.offer("one");
+        queue.offer("two");
+        queue.offer("three");
+
+        System.out.println("----------start------------");
+        System.out.println(queue.poll());
+        System.out.println(queue.poll());
+        System.out.println(queue.poll());
+        System.out.println("----------stop------------");
+    }
+}
