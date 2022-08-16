5f7509e5087d65653c343f3cf43f787fe1339f11
chengsukai <1710290870@qq.com>
2022-08-16 17:02:00 +0800

    chore: 进程间通信

---
+++ src/main/java/sukai/currency/caseVIII/_04_force_sleep.java
@@ -0,0 +1,40 @@
+package sukai.currency.caseVIII;
+
+import sukai.currency.caseVI.SmallTool;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author chengsukai
+ * @since 2022-08-16 15:47
+ */
+public class _04_force_sleep {
+    public static void main(String[] args) {
+        Thread thread = new Thread(() -> {
+            SmallTool.printTimeAndThread("开始睡眠");
+            forceSleep(10);
+            SmallTool.printTimeAndThread("结束睡眠");
+        });
+        thread.start();
+        thread.interrupt();
+    }
+
+    public static void forceSleep(int second) {
+        long startTime = System.currentTimeMillis();
+        long sleepMills = TimeUnit.SECONDS.toMillis(second);
+
+        while ((startTime + sleepMills) > System.currentTimeMillis()) {
+            long sleepTime = startTime + sleepMills - System.currentTimeMillis();
+            if (sleepTime <= 0) {
+                break;
+            }
+            try {
+//                抛出异常之前会清除线程的中断标识
+                Thread.sleep(sleepTime);
+            } catch (InterruptedException e) {
+//                SmallTool.printTimeAndThread("发生中断");
+            }
+        }
+    }
+
+}
---
+++ src/main/java/sukai/currency/caseVIII/_TwoCarCrossBridge.java
@@ -0,0 +1,37 @@
+package sukai.currency.caseVIII;
+
+import sukai.currency.caseVI.SmallTool;
+
+import java.util.Random;
+
+/**
+ * @author chengsukai
+ * @since 2022-08-16 15:37
+ */
+public class _TwoCarCrossBridge {
+    public static void main(String[] args) {
+        Thread carTwo = new Thread(() -> {
+            SmallTool.printTimeAndThread("卡丁2号 准备过桥");
+            SmallTool.printTimeAndThread("发现1号在过，开始等待");
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                SmallTool.printTimeAndThread("卡丁2号 开始过桥");
+            }
+            SmallTool.printTimeAndThread("卡丁二号 过桥完毕");
+        });
+
+        Thread carOne = new Thread(() -> {
+            SmallTool.printTimeAndThread("卡丁1号 准备过桥");
+            int timeSpend = new Random().nextInt(500) + 1000;
+            SmallTool.sleepMills(timeSpend);
+            SmallTool.printTimeAndThread("卡丁1号 过桥完毕，耗时：" + timeSpend);
+
+            // 卡丁1号 中断了 卡丁2号
+            carTwo.interrupt();
+        });
+
+        carOne.start();
+        carTwo.start();
+    }
+}
