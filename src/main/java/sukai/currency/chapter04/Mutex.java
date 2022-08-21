42152371aebf8576d3ef27431eef96f8d9295f65
chengsukai <1710290870@qq.com>
2022-08-21 23:38:07 +0800

    learn: synchronized同步

---
+++ src/main/java/sukai/currency/chapter04/Mutex.java
@@ -0,0 +1,28 @@
+package sukai.currency.chapter04;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author chengsukai
+ * @since 2022-08-18 13:54
+ */
+public class Mutex {
+    private final static Object MUTEX = new Object();
+
+    public void accessResource() {
+        synchronized (MUTEX) {
+            try {
+                TimeUnit.MINUTES.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        final Mutex mutex = new Mutex();
+        for (int i = 0; i < 5; i++) {
+            new Thread(mutex::accessResource).start();
+        }
+    }
+}
