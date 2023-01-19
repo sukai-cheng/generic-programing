a43e3a3fafd997dd83a93254b190e8d337706a84
chengsukai <chengsukai@gmail.com>
2023-01-20 00:32:39 +0800

    java8-将函数作为值进行传递

---
+++ src/main/java/sukai/java8/chapter01/AppleTest.java
@@ -0,0 +1,59 @@
+package sukai.java8.chapter01;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class AppleTest {
+    public static boolean isGreenApple(Apple apple) {
+        return "green".equals(apple.getColor());
+    }
+
+    public static boolean isHeavyApple(Apple apple) {
+        return apple.getWeight() > 150;
+    }
+
+    public static List<Apple> filterGreenApples(List<Apple> inventory) {
+        List<Apple> result = new ArrayList<>();
+        for (Apple apple : inventory) {
+            if ("green".equals(apple.getColor())) {
+                result.add(apple);
+            }
+        }
+        return result;
+    }
+
+    public static List<Apple> filterHeavyApples(List<Apple> inventory) {
+        List<Apple> result = new ArrayList<>();
+        for (Apple apple : inventory) {
+            if (apple.getWeight() > 150) {
+                result.add(apple);
+            }
+        }
+        return result;
+    }
+
+    static List<Apple> filterApples(List<Apple> inventory,
+                                    Predicate<Apple> p) {
+        List<Apple> result = new ArrayList<>();
+        for (Apple apple : inventory) {
+            if (p.test(apple)) {
+                result.add(apple);
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        List<Apple> inventory = Arrays.asList(new Apple(80, "green"),
+                new Apple(155, "green"),
+                new Apple(120, "red"));
+
+        List<Apple> result = filterApples(inventory, AppleTest::isGreenApple);
+
+        System.out.println(result);
+
+
+    }
+}
