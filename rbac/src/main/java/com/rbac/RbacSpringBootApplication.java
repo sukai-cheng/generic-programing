3c48b5d8a2477cc01b6f52655c794fd50e6b9524
chengsukai <chengsukai@gmail.com>
2023-01-27 18:46:23 +0800

    springboot降级，mybatis plus 3.5.3.1不支持spring3.0, 降级至3.5.2

--- rbac/src/main/java/com/rbac/RbacSpringBootApplication.java
+++ rbac/src/main/java/com/rbac/RbacSpringBootApplication.java
@@ -4,7 +4,6 @@ import com.github.jeffreyning.mybatisplus.conf.EnableMPP;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 //@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @SpringBootApplication
--- rbac/src/main/java/com/rbac/config/JwtAuthenticationTokenFilter.java
+++ rbac/src/main/java/com/rbac/config/JwtAuthenticationTokenFilter.java
@@ -4,17 +4,17 @@ import com.rbac.entity.LoginUser;
 import com.rbac.utils.JwtUtil;
 import com.rbac.utils.RedisCache;
 import io.jsonwebtoken.Claims;
+import jakarta.annotation.Resource;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import javax.annotation.Resource;
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Objects;
 
