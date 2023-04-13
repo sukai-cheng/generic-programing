a5675735a1664eca46b110e76b3c4df9e4e99d58
chengsukai <chengsukai@gmail.com>
2023-04-13 14:09:01 +0800

    开放静态资源文件访问

---
+++ spring-boot-concurrency/kill-system/src/main/java/com/seckill/config/WebConfig.java
@@ -0,0 +1,37 @@
+package com.seckill.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Resource
+    private UserArgumentResolver userArgumentResolver;
+
+    /**
+     * 自定义参数
+     */
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(userArgumentResolver);
+    }
+
+    /**
+     * 开放静态资源文件
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/META-INF/resources/")
+                .addResourceLocations("classpath:/resources/").addResourceLocations("classpath:/static/")
+                .addResourceLocations("classpath:/public/");
+        WebMvcConfigurer.super.addResourceHandlers(registry);
+    }
+}
