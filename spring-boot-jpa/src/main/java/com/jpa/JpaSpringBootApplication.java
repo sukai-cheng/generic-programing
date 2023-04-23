925e6afd993cd4a1d42b708b0ef7d8e7f2092226
chengsukai <chengsukai@gmail.com>
2023-04-23 11:24:45 +0800

    创建工程

--- spring-boot-jpa/src/main/java/com/jpa/JpaSpringBootApplication.java
+++ spring-boot-jpa/src/main/java/com/jpa/JpaSpringBootApplication.java
@@ -2,12 +2,12 @@ package com.jpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+//@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 public class JpaSpringBootApplication {
     public static void main(String[] args) {
         SpringApplication.run(JpaSpringBootApplication.class, args);
--- spring-boot-kafka/src/test/java/SpringBootKafkaApplicationTests.java
+++ spring-boot-kafka/src/test/java/SpringBootKafkaApplicationTests.java
@@ -1,30 +1,30 @@
-import com.kafka.KafkaSpringBootApplication;
-import com.kafka.provider.KafkaProvider;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
-
-import javax.annotation.Resource;
-import java.time.LocalDateTime;
-import java.util.UUID;
-import java.util.concurrent.TimeUnit;
-
-@ComponentScan(basePackages = "com.kafka")
-@SpringBootTest(classes = KafkaSpringBootApplication.class)
-public class SpringBootKafkaApplicationTests {
-
-    @Resource
-    private KafkaProvider kafkaProvider;
-
-    @Test
-    public void sendMessage() throws InterruptedException{
-        for(int i = 0; i < 1000; i++){
-            long orderId = i + 1;
-            String orderNum = UUID.randomUUID().toString();
-            kafkaProvider.sendMessage(orderId,orderNum, LocalDateTime.now());
-        }
-
-        TimeUnit.MINUTES.sleep(1);
-    }
-
-}
+//import com.kafka.KafkaSpringBootApplication;
+//import com.kafka.provider.KafkaProvider;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.context.annotation.ComponentScan;
+//
+//import javax.annotation.Resource;
+//import java.time.LocalDateTime;
+//import java.util.UUID;
+//import java.util.concurrent.TimeUnit;
+//
+//@ComponentScan(basePackages = "com.kafka")
+//@SpringBootTest(classes = KafkaSpringBootApplication.class)
+//public class SpringBootKafkaApplicationTests {
+//
+//    @Resource
+//    private KafkaProvider kafkaProvider;
+//
+//    @Test
+//    public void sendMessage() throws InterruptedException{
+//        for(int i = 0; i < 1000; i++){
+//            long orderId = i + 1;
+//            String orderNum = UUID.randomUUID().toString();
+//            kafkaProvider.sendMessage(orderId,orderNum, LocalDateTime.now());
+//        }
+//
+//        TimeUnit.MINUTES.sleep(1);
+//    }
+//
+//}
