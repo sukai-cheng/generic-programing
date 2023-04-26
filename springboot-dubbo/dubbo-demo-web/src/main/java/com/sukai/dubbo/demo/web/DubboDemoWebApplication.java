f041870a90bb9ac504048fb6b0db550f2c63c9fc
chengsukai <chengsukai@gmail.com>
2023-04-26 08:28:52 +0500

    配置dubbo-qos配置

--- springboot-dubbo/dubbo-demo-web/src/main/java/com/sukai/dubbo/demo/web/DubboDemoWebApplication.java
+++ springboot-dubbo/dubbo-demo-web/src/main/java/com/sukai/dubbo/demo/web/DubboDemoWebApplication.java
@@ -3,6 +3,9 @@ package com.sukai.dubbo.demo.web;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.system.ApplicationHome;
+
+import java.io.File;
 
 /**
  * @author chengsukai
@@ -11,6 +14,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @EnableDubbo
 public class DubboDemoWebApplication {
     public static void main(String[] args) {
+        ApplicationHome home = new ApplicationHome(DubboDemoWebApplication.class);
+        File jarFile = home.getSource();
+        String dirPath = jarFile.getParentFile().toString();
+        String filePath = dirPath + File.separator + ".dubbo";
+        System.out.println(filePath);
+
+        System.setProperty("dubbo.meta.cache.filePath", filePath);
+        System.setProperty("dubbo.mapping.cache.filePath",filePath);
         SpringApplication.run(DubboDemoWebApplication.class, args);
     }
 }
