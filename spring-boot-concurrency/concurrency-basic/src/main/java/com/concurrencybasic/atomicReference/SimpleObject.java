319f7feabd4ffae6f2f770736dbc0c0cd5fa993f
chengsukai <chengsukai@gmail.com>
2023-05-18 15:05:34 +0800

    项目: springboot-saToken 内容: 原子类测试

---
+++ spring-boot-concurrency/concurrency-basic/src/main/java/com/concurrencybasic/atomicReference/SimpleObject.java
@@ -0,0 +1,20 @@
+package com.concurrencybasic.atomicReference;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * @author chengsukai
+ */
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class SimpleObject {
+    private String name;
+    private Integer age;
+
+
+}
