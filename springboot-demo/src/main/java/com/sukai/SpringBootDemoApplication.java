0ceae74ee110d1a59278ecb39974e1160448b6a7
sukai-cheng <1710290860@qq.com>
2024-02-25 15:57:35 +0800

    servlet异步请求导致的问题

--- springboot-demo/src/main/java/com/sukai/SpringBootDemoApplication.java
+++ springboot-demo/src/main/java/com/sukai/SpringBootDemoApplication.java
@@ -3,12 +3,13 @@ package com.sukai;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 /**
  * @author chengsukai
  */
 @SpringBootApplication
-@MapperScan("com.sukai.mapper")
+@ServletComponentScan
 public class SpringBootDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDemoApplication.class, args);
