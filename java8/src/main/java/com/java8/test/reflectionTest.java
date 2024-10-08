1810983d3f3bab23c4fc4521a358b2641a1df577
chengsk <chengsk@jsalpha.com>
2024-10-08 08:10:36 +0800

    反射测试

---
+++ java8/src/main/java/com/java8/test/reflectionTest.java
@@ -0,0 +1,8 @@
+package com.java8.test;
+
+public class reflectionTest {
+    public static void main(String[] args) {
+        Class<?> aClass = "foo".getClass();
+        System.out.println(aClass);
+    }
+}
